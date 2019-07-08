4c648f53b68e27d2d6aa91bf1b6677be6b1a5247
Aaron Blum <blumaa@gmail.com>
2019-07-08 23:21:33 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -8,22 +8,22 @@ def display_board(board)
   puts " #{board[6]} | #{board[7]} | #{board[8]} "
 end
 
-def valid_move?(board, user_input)
-  if position_taken?(board, user_input) == false && user_input.between?(0,8) == true
+def valid_move?(board, index)
+  if position_taken?(board, index) == false && index.between?(0,8) == true
     true
   end
 end
 
-def position_taken?(board, user_input)
-  if board[user_input] == " "
+def position_taken?(board, index)
+  if board[index] == " "
     false
-  elsif board[user_input] == ""
+  elsif board[index] == ""
     false
-  elsif board[user_input] == "X"
+  elsif board[index] == "X"
     true
-  elsif board[user_input] == "O"
+  elsif board[index] == "O"
     true
-  elsif board[user_input] == nil
+  elsif board[index] == nil
     false
   else
   end
@@ -33,8 +33,8 @@ def input_to_index(user_input)
   user_input.to_i - 1
 end
 
-def move(board, user_input, user_input = "X")
-  board[user_input] = user_input
+def move(board, index, user_input = "X")
+  board[index] = user_input
 end
 
 def turn(board)
