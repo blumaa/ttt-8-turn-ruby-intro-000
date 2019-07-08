89e530b78a552a134e2bccc2b876473cbcd1e10e
Aaron Blum <blumaa@gmail.com>
2019-07-08 22:55:02 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -9,7 +9,9 @@ def display_board(board)
 end
 
 def valid_move?(board, index)
-
+  if position_taken?(board, index) == false && index.between?(0,8) == true
+    true
+  end
 end
 
 def position_taken?(board, index)
