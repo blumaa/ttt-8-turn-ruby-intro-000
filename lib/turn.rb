68b4e52093729eacf11727ef446526a65c4272e1
Aaron Blum <blumaa@gmail.com>
2019-07-08 22:49:02 +0000

    Automatically backed up by Learn

---
+++ lib/turn.rb
@@ -0,0 +1,17 @@
+display_board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
+
+def display_board(board, index, value)
+
+end
+
+def valid_move?(board, index)
+
+end
+
+def position_taken?(board, index)
+
+end
+
+def move(board, index, value)
+
+end
